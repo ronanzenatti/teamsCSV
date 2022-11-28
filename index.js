1c46980c1196dff8b9222d7700b23bcddeb83669
ronanzenatti <ronanzenatti@gmail.com>
2022-11-28 13:05:11 -0300

    att

--- index.js
+++ index.js
@@ -5,7 +5,19 @@ const { readdir, stat } = require('fs').promises;
 const { sep } = require('path');
 const xlsx = require("node-xlsx");
 
-const path = '/Users/ronan/OneDrive - Centro Paula Souza/Google/8-2022/Material de Aula/CHAMADAS';
+const readline = require('readline');
+
+const paths = [
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/Cisco/07-2022/Material de Aula/Chamadas_07-2022', file: 'CCNA2_07-2022' },
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/Cisco/12-2022/LISTAS_PRESENÇA', file: 'CCNA1_12-2022' },
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/Google/8-2022/Material de Aula/CHAMADAS', file: 'GCCF_08-2022' },
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/Microsoft/10-2022/Lista de Presença', file: 'MICROSOFT_10-2022' },
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/Salesforce/P11-2022/LISTA DE PRESENÇA', file: 'SALESFORCE_11-2022' },
+    { path: '/Users/ronan/OneDrive - Centro Paula Souza/SIXSIGMA/06-2022/CHAMADAS', file: 'SIXSIGMA_06-2022' },
+];
+
+let indice = 0;
+let filename = '';
 
 const chamadas = [];
 let turma = {};
@@ -17,7 +29,13 @@ let nomeAnterior = '';
 let ultimo = 0;
 
 let i = 0;
+let linha = 0;
+let resumo = false;
 
+var leitor = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function exportacao(planilha) {
     let exportacao = [];
@@ -36,7 +54,7 @@ function exportacao(planilha) {
     console.log(` -> Iniciando a Gravação do arquivo:`);
     var buffer = xlsx.build(exportacao); // Returns a buffer
 
-    fs.writeFile(`GOOGLE.xlsx`, buffer);
+    fs.writeFile(`${filename}-02-11-22.xlsx`, buffer);
     console.log(` -> Arquivo gravado com sucesso!`);
 
 }
@@ -85,8 +103,11 @@ function montarExcel() {
 async function processar() {
 
     const dadosArquivo = await fs.readFile(arquivos[i]);
-    const nomeTurma = arquivos[i].split('\\')[1];
+    let nomeTurma = arquivos[i].split('\\')[1];
     const nomeArquivo = arquivos[i].split('\\')[2];
+    // console.log(nomeArquivo);
+
+    nomeTurma = nomeTurma.toString().substr(0, 29);
 
     if (nomeAnterior == nomeTurma || nomeAnterior == '') {
 
@@ -100,13 +121,12 @@ async function processar() {
 
     var parser = parse(dadosArquivo,
         {
-            delimiter: ["\t"],
+            delimiter: ["\t", ","],
             encoding: 'utf16le',
             // skip_empty_lines: true,
             relaxColumnCount: true,
             relaxQuotes: true,
         }, function (err, records) {
-
             let alunos = false;
             let dia = true
 
@@ -114,20 +134,36 @@ async function processar() {
             const participantes = [];
 
             let type = 0;
+            linha = 0;
 
             records.map(item => {
+
                 const valor = item[0].split('\t');
 
-                if (valor[0].startsWith('﻿Nom') || valor[0].startsWith('Ful') || valor[0].startsWith('Nom') || valor[0].startsWith('﻿Ful')) {
+                if (valor[0].startsWith('Res') && linha == 0)
+                    resumo = true;
+                else if (linha == 0 && !valor[0].startsWith('Res'))
+                    resumo = false;
+
+                linha++;
+
+                if (valor[0].startsWith('﻿Nom') || valor[0].startsWith('Ful') || valor[0].startsWith('Nom') || valor[0].startsWith('﻿Ful') || valor[0].startsWith('\nNom') || valor[0].startsWith('Nam')) {
                     alunos = true;
-                    if (valor[0].startsWith('﻿Nom') || valor[0].startsWith('﻿Ful'))
-                        type = 1;
-                    else if (valor[0].startsWith('Ful'))
+
+                    // if (nomeArquivo == "(25-09-22) meetingAttendanceReport.csv")
+                    //   console.log(valor[0])
+
+                    if (valor[0].startsWith('﻿Nom') || valor[0].startsWith('﻿Ful')) {
+                        type = (resumo) ? 1 : 4;
+                    } else if (valor[0].startsWith('Ful'))
                         type = 2;
+                    else if (valor[0].startsWith('Nom') && resumo)
+                        type = 4;
                     else
                         type = 3;
 
                 } else if (alunos) {
+                    valor[0] = valor[0].toString().trim();
 
                     if (!valor[0]) {
                         alunos = false;
@@ -138,8 +174,15 @@ async function processar() {
                     }
 
                     if (dia) {
+                        if (nomeArquivo == "Aula 01-09-2022.csv")
+                            console.log(nomeArquivo, item[2])
+
                         dia = false;
-                        if (type == 1 || type == 3) {
+                        if (type == 4) {
+                            data.dia = (resumo) ? item[1].split(' ')[0].split(',')[0] : item[2].split(' ')[0].split(',')[0];
+                            if (nomeArquivo == "Aula 01-09-2022.csv")
+                                console.log(data.dia)
+                        } else if (type == 1 || type == 3) {
                             data.dia = item[2].split(' ')[0].split(',')[0];
                         } else {
                             data.dia = item[1].split(' ')[0].split(',')[0];
@@ -190,10 +233,20 @@ async function listarArquivosDoDiretorio(diretorio, arquivos) {
 }
 
 async function buscarArquivos() {
-    arquivos = await listarArquivosDoDiretorio(path);
-    ultimo = arquivos.length;
 
-    processar();
+    leitor.question("Qual planilha deseja?\n 0 - CCNA 2\n 1 - CCNA 1\n 2 - GCCF\n 3 - Microsoft\n 4 - Salesforce\n 5 - Six Sigma\n", async function (answer) {
+        indice = answer;
+        console.log("\nVocê escolheu: " + indice);
+        leitor.close();
+
+        arquivos = await listarArquivosDoDiretorio(paths[indice].path);
+        ultimo = arquivos.length;
+        filename = paths[indice].file;
+
+        processar();
+
+    });
+
 
 }
 
